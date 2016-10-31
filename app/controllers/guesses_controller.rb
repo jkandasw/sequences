3a55af922d2a1573bdc3754e95406a5c531298b7
JayaS <JayaS@users.noreply.github.com>
2016-10-31 11:37:58 -0500

    Changes

---
+++ app/controllers/guesses_controller.rb
@@ -0,0 +1,31 @@
+class GuessesController < ApplicationController
+  def index
+    @user_input_1 = params["first_number"]
+    @user_input_2 = params["second_number"]
+    @user_input_3 = params["third_number"]
+
+g=Guess.new
+g.first_num = params["first_number"]
+g.second_num = params["second_number"]
+g.third_num = params["third_number"]
+g.save
+
+    @list = Guess.all
+
+
+
+       if @user_input_1<@user_input_2 && @user_input_2<@user_input_3 && @user_input_1<@user_input_3
+      @check="Yes"
+    elsif @check="No"
+
+    end
+    render("/guesses/index.html.erb")
+  end
+
+  def answer
+
+
+@user_answer = params["rule"]
+    render("/guesses/answer.html.erb")
+  end
+end
