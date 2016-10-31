9e626e4a097bd2d77ab75c00e1ffb2ab0b598b00
JayaS <JayaS@users.noreply.github.com>
2016-10-31 12:41:47 -0500

    changes

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -4,28 +4,33 @@ class GuessesController < ApplicationController
     @user_input_2 = params["second_number"]
     @user_input_3 = params["third_number"]
 
-g=Guess.new
-g.first_num = params["first_number"]
-g.second_num = params["second_number"]
-g.third_num = params["third_number"]
-g.save
+    g=Guess.new
+    g.first_num = params["first_number"]
+    g.second_num = params["second_number"]
+    g.third_num = params["third_number"]
+    g.save
 
     @list = Guess.all
 
 
+    if @user_input_1=@user_input_2=@user_input_3==nil
+             @user_input_1=1
+             @user_input_2=2
+             @user_input_3=3
+    end
 
-       if @user_input_1<@user_input_2 && @user_input_2<@user_input_3 && @user_input_1<@user_input_3
+    if @user_input_1<@user_input_2 && @user_input_2<@user_input_3 && @user_input_1<@user_input_3
       @check="Yes"
     elsif @check="No"
-
     end
+
     render("/guesses/index.html.erb")
   end
 
   def answer
 
 
-@user_answer = params["rule"]
+    @user_answer = params["rule"]
     render("/guesses/answer.html.erb")
   end
 end
